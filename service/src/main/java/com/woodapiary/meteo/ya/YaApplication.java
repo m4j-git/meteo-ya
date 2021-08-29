0336be8901d54dd10f9329f0b8172754cf1d0946
woodapiary <git@woodapiary.com>
2021-08-29 21:22:13 +0300

    add embeded tomcat to meteo-admin

--- service/src/main/java/com/woodapiary/meteo/ya/YaApplication.java
+++ service/src/main/java/com/woodapiary/meteo/ya/YaApplication.java
@@ -3,8 +3,6 @@
  */
 package com.woodapiary.meteo.ya;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
