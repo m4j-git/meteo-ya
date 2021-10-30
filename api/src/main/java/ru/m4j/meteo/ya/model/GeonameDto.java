edd9de5a9197a314e8dbd255a24a922e3df02ec4
woodapiary <git@woodapiary.com>
2021-10-30 19:33:16 +0300

    fix geoname

--- api/src/main/java/ru/m4j/meteo/ya/model/GeonameDto.java
+++ api/src/main/java/ru/m4j/meteo/ya/model/GeonameDto.java
@@ -23,8 +23,9 @@ public class GeonameDto implements Serializable {
 
     private Integer geonameId;
     private String geoname;
-    private Double lon;
     private Double lat;
+    private Double lon;
+
 
     @Override
     public String toString() {
