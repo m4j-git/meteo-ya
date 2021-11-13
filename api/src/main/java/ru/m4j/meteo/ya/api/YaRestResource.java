751cba215c219925ea010c21c0daa258234b5540
m4j <git@m4j.ru>
2021-11-13 05:03:14 +0300

    set rest only via json

--- api/src/main/java/ru/m4j/meteo/ya/api/YaRestResource.java
+++ api/src/main/java/ru/m4j/meteo/ya/api/YaRestResource.java
@@ -5,6 +5,7 @@ package ru.m4j.meteo.ya.api;
 
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,24 +15,24 @@ import io.swagger.annotations.ApiOperation;
 import ru.m4j.meteo.ya.model.YaFactDto;
 import ru.m4j.meteo.ya.model.YaMessageDto;
 
-@RequestMapping("/api/v1/ya")
+@RequestMapping("/api/v1")
 public interface YaRestResource {
 
     @ApiOperation(notes = "Yandex messages", value = "get list of weather messages")
-    @GetMapping("/messages")
+    @GetMapping(value = "/messages", produces = MediaType.APPLICATION_JSON_VALUE)
     List<YaMessageDto> getMessages(@RequestParam Integer geonameId, @RequestParam(required = false) String dateFrom,
             @RequestParam(required = false) String dateTo);
 
     @ApiOperation(notes = "Yandex messages", value = "get weather message")
-    @GetMapping("/messages/{uuid}")
+    @GetMapping(value = "/messages/one/{uuid}", produces = MediaType.APPLICATION_JSON_VALUE)
     YaMessageDto getMessage(@PathVariable(value = "uuid") String uuid);
 
     @ApiOperation(notes = "Yandex messages", value = "get last weather message")
-    @GetMapping("/messages/last")
+    @GetMapping(value = "/messages/last", produces = MediaType.APPLICATION_JSON_VALUE)
     YaMessageDto getLastMessage(@RequestParam Integer geonameId);
 
     @ApiOperation(notes = "Yandex messages", value = "get fact weather messages")
-    @GetMapping("/messages/facts")
+    @GetMapping(value = "/messages/facts", produces = MediaType.APPLICATION_JSON_VALUE)
     List<YaFactDto> getFacts(@RequestParam Integer geonameId, @RequestParam(required = false) String dateFrom,
             @RequestParam(required = false) String dateTo);
 }
