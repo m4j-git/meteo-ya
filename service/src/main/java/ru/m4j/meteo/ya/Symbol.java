e04df9146bd3b33f1065d59262c5437e59518387
alex <alex@localhost>
2021-12-01 15:05:47 +0300

    add aux clases

---
+++ service/src/main/java/ru/m4j/meteo/ya/Symbol.java
@@ -0,0 +1,11 @@
+package ru.m4j.meteo.ya;
+
+public final class Symbol {
+
+    public static final String SPACE = " ";
+    public static final String COMMA = ",";
+    public static final String PERCENTAGE = "%";
+
+    private Symbol() {
+    }
+}
