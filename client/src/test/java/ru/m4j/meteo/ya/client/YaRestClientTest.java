8a7c4c3d43fd3c679a552a5ec6f01e34ef2cb022
m4j <git@m4j.ru>
2022-01-04 21:25:28 +0300

    read test data from classpath resource

--- client/src/test/java/ru/m4j/meteo/ya/client/YaRestClientTest.java
+++ client/src/test/java/ru/m4j/meteo/ya/client/YaRestClientTest.java
@@ -7,11 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,19 +27,17 @@ import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import ru.m4j.meteo.share.app.GlobalConstants;
 import ru.m4j.meteo.ya.client.YaRestClientTest.RestClientTestConfig;
 import ru.m4j.meteo.ya.model.YaFactDto;
 import ru.m4j.meteo.ya.model.YaMessageDto;
+import ru.m4j.meteo.ya.srv.config.YaTestBeanSource;
 
 @RestClientTest(YaRestClientImpl.class)
 @AutoConfigureWebClient(registerRestTemplate = true)
-@ContextConfiguration(classes = { YaRestClientImpl.class })
+@ContextConfiguration(classes = { YaRestClientImpl.class, YaTestBeanSource.class })
 @Import(RestClientTestConfig.class)
 @DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 class YaRestClientTest {
-
-    private static final String TEST_DATA_FILE = "ya_v1.json";
 
     private final Integer geonameId = 1;
 
@@ -53,17 +47,12 @@ class YaRestClientTest {
     private MockRestServiceServer server;
     @Autowired
     private ObjectMapper jacksonMapper;
-
-    private YaMessageDto readJson() throws IOException {
-        final FileInputStream fis = new FileInputStream(GlobalConstants.TEST_DATA_PATH + TEST_DATA_FILE);
-        try (BufferedReader rd = new BufferedReader(new InputStreamReader(fis, StandardCharsets.UTF_8))) {
-            return jacksonMapper.readValue(rd, YaMessageDto.class);
-        }
-    }
+    @Autowired
+    private YaTestBeanSource src;
 
     @Test
     void whenCallingGetFacts_thenClientMakesCorrectCall() throws IOException {
-        YaMessageDto dto = readJson();
+        YaMessageDto dto = src.readJson();
         List<YaFactDto> factList = new ArrayList<>();
         factList.add(dto.getFact());
         String json = jacksonMapper.writeValueAsString(factList);
@@ -76,7 +65,7 @@ class YaRestClientTest {
 
     @Test
     void whenCallingGetLastMessage_thenClientMakesCorrectCall() throws IOException {
-        YaMessageDto dto = readJson();
+        YaMessageDto dto = src.readJson();
         String json = jacksonMapper.writeValueAsString(dto);
 
         server.expect(requestTo(client.getUri("messages/last", geonameId)))
@@ -87,7 +76,7 @@ class YaRestClientTest {
 
     @Test
     void whenCallingGetMessages_thenClientMakesCorrectCall() throws IOException {
-        YaMessageDto dto = readJson();
+        YaMessageDto dto = src.readJson();
         YaMessageDto[] mesList = new YaMessageDto[] { dto };
         String json = jacksonMapper.writeValueAsString(mesList);
         server.expect(requestTo(client.getUri("messages", geonameId)))
--- service/src/test/java/ru/m4j/meteo/ya/srv/config/YaTestBeanSource.java
+++ service/src/test/java/ru/m4j/meteo/ya/srv/config/YaTestBeanSource.java
@@ -3,22 +3,16 @@
  */
 package ru.m4j.meteo.ya.srv.config;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import lombok.extern.slf4j.Slf4j;
-import ru.m4j.meteo.share.app.GlobalConstants;
 import ru.m4j.meteo.ya.model.YaMessageDto;
 
-@Slf4j
 @Component
 public class YaTestBeanSource {
 
@@ -28,12 +22,8 @@ public class YaTestBeanSource {
     private ObjectMapper jacksonMapper;
 
     public YaMessageDto readJson() throws IOException {
-        final FileInputStream fis = new FileInputStream(GlobalConstants.TEST_DATA_PATH + TEST_DATA_FILE);
-        try (BufferedReader rd = new BufferedReader(new InputStreamReader(fis, StandardCharsets.UTF_8))) {
-            YaMessageDto dto = jacksonMapper.readValue(rd, YaMessageDto.class);
-            log.debug("read from json " + dto);
-            return dto;
-        }
+        return jacksonMapper.readValue((new ClassPathResource("data/" + TEST_DATA_FILE)).getInputStream(), YaMessageDto.class);
+
     }
 
 }
